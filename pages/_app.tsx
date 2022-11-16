cd72f726e6ed1e11b98532c2453baf472fc690fb
Isidora Wright <iwright@DESKTOP-S8HFU1T.localdomain>
2022-11-15 21:02:19 -0600

    google maps functioning but it's on both client and manager views

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,6 @@ let googleTranslateElementInit = once(() => {
   );
 });
 
-
 export default function MyApp(props: any) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   React.useEffect(() => {
@@ -42,6 +41,9 @@ export default function MyApp(props: any) {
         <NavBar />
         <Component {...pageProps} />
         <div id="google_translate_element"></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3433.7667023268996!2d-96.34363518513555!3d30.612342998476418!
+          2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864683afcd3de05f%3A0xf78e5c7be3131946!2sSpin%20&#39;N%20Stone%20Pizza%20-%20MSC!
+          5e0!3m2!1sen!2sus!4v1668567572807!5m2!1sen!2sus" width="600" height="450" loading="lazy"></iframe>
       </ThemeProvider>
     </CacheProvider>
   );
